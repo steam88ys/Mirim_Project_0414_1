1d062a8eb0ffd016aa9f3b2ea5a831ea63b97e5a
steam88ys <yssteam88@gmail.com>
2022-04-19 19:36:07 +0900

    라디오버튼에 따라 이미지 변경

--- app/src/main/java/kr/hs/emirim/s2102/mirim_project_0414_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2102/mirim_project_0414_1/MainActivity.java
@@ -25,10 +25,27 @@ public class MainActivity extends AppCompatActivity {
         checkStart = findViewById(R.id.check_start);
         imgv = findViewById(R.id.imgv);
         Button btnDone = findViewById(R.id.btn_done);
-
+        btnDone.setOnClickListener(btnListener);
         checkStart.setOnCheckedChangeListener(checkListener);
 
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (rg.getCheckedRadioButtonId()) {
+                case R.id.rb_dog:
+                    imgv.setImageResource(R.drawable.dog);
+                    break;
+                case R.id.rb_cat:
+                    imgv.setImageResource(R.drawable.cat);
+                    break;
+                case R.id.rb_rabbit:
+                    imgv.setImageResource(R.drawable.rabbit);
+                    break;
+            }
+        }
+    };
 
     CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener() {
         @Override
